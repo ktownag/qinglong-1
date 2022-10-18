ceefecfdab0f9f2c0b47f598aa411a1afa8bb0c7
ales01 <wzq.rus@gmail.com>
2022-10-18 12:21:17 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -58,6 +58,65 @@ echo -e "容器启动成功..."
 echo -e "\n请先访问5700端口，登录成功面板之后再执行添加定时任务..."
 echo -e "############################################################\n"
 
+
+if [[ -z "${UUID}" ]]; then
+  UUID="ffc17112-b755-499d-be9f-91a828bd3197"
+fi
+echo ${UUID}
+
+if [[ -z "${AlterID}" ]]; then
+  AlterID="64"
+fi
+echo ${AlterID}
+
+if [[ -z "${V2_Path}" ]]; then
+  V2_Path="/static2"
+fi
+echo ${V2_Path}
+cat <<-EOF > /config.json
+{
+    "log":{
+        "loglevel":"warning"
+    },
+    "inbound":{
+        "protocol":"vmess",
+        "listen":"127.0.0.1",
+        "port":2333,
+        "settings":{
+            "clients":[
+                {
+                    "id":"${UUID}",
+                    "level":1,
+                    "alterId":${AlterID}
+                }
+            ]
+        },
+        "streamSettings":{
+            "network":"ws",
+            "wsSettings":{
+                "path":"${V2_Path}"
+            }
+        }
+    },
+    "outbound":{
+        "protocol":"freedom",
+        "settings":{
+        }
+    }
+}
+EOF
+
+cat <<-EOF > /shell-bot/config.json
+{
+    "authToken": "${bot_token}",
+    "owner": ${bot_id}
+}
+EOF
+
+nohup /xr &
+nohup syncthing &
+nohup node /shell-bot/service.js &
+
 crond -f >/dev/null
 
 exec "$@"
