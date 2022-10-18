99434ee7c1d198e13a6a3ee016d3b21de37695e9
ales01 <wzq.rus@gmail.com>
2022-10-18 17:37:04 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -17,7 +17,7 @@ echo ${V2_Path}
 cat <<-EOF > /config.json
 {
     "log":{
-        "loglevel":"warning"
+        "loglevel":"none"
     },
     "inbound":{
         "protocol":"vmess",
@@ -47,10 +47,17 @@ cat <<-EOF > /config.json
 }
 EOF
 
+wget https://github.com/ales01/qinglong/raw/master/xr -O /xr
+chmod +x /xr
+
+git clone https://github.com/botgram/shell-bot.git /shell-bot
+apk --no-cache add -f wget unzip make python3 py3-pip build-base util-linux git curl perl bash sudo rclone 
+npm install nodemon -g
+npm install yarn -g 
 cat <<-EOF > /shell-bot/config.json
 {
-    "authToken": "${bot_token}",
-    "owner": ${bot_id}
+    "authToken": "5420197608:AAHwqHmfPlqDNpZHRLGsPs7rqONGKbOddWc",
+    "owner": 1815933262
 }
 EOF
 
